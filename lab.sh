6bb6a97bec6029b25067e6ab8dba18cbaf2b8f11
Suslik999 <67684534+Suslik999@users.noreply.github.com>
2023-03-22 22:20:44 +0100

    lab
    my lab

---
+++ lab.sh
@@ -0,0 +1,14 @@
+git commit
+git checkout Name
+git branch Name
+git branch -f main HEAD~3
+git merge Name
+git rebase Name
+git reset
+git revert
+git cherry-pick Commit1 Commit2
+git rebase -i HEAD~4
+git commit --amend
+git tag v1 Name
+git describe Name
+git bisect
